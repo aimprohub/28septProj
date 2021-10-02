2e19465e863be5aa6adb47252c5cdfc4c39899af
AIMsnet <girishm32@yahoo.com>
2021-10-02 13:11:20 +0530

    Second commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/MILKBANK_CPY'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/lacto/' }),
+    res.sendFile('index.html', { root: 'dist/MILKBANK_CPY/' }),
 );
 
 // Start the app by listening on the default Heroku port
